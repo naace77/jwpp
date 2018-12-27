cb62573afac5edffddc5d6d1a79c4b7f5a4d9e69
jongwoo <naace910@gmail.com>
2018-12-27 15:56:58 +0900

    Updates

---
+++ precache-manifest.25c6014c04a5f3f45c5180004d246ff7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3705a92f60cdd1d25fd8",
+    "url": "/jwpp/static/js/runtime~main.3705a92f.js"
+  },
+  {
+    "revision": "bf619ac10bb8d1b38795",
+    "url": "/jwpp/static/js/main.bf619ac1.chunk.js"
+  },
+  {
+    "revision": "8ec7636ebbdd1185797c",
+    "url": "/jwpp/static/js/1.8ec7636e.chunk.js"
+  },
+  {
+    "revision": "bf619ac10bb8d1b38795",
+    "url": "/jwpp/static/css/main.043657a9.chunk.css"
+  },
+  {
+    "revision": "190bfab21fab78fea06b60760c853901",
+    "url": "/jwpp/index.html"
+  }
+];
