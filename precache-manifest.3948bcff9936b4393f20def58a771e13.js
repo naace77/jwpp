d2f8a78e720d6798ad7584a0b71cb2b1c0cec777
jongwoo <naace910@gmail.com>
2018-12-31 16:38:59 +0900

    Updates

--- precache-manifest.3948bcff9936b4393f20def58a771e13.js
+++ precache-manifest.3948bcff9936b4393f20def58a771e13.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/jwpp/static/js/runtime~main.3705a92f.js"
   },
   {
-    "revision": "337beaf638115907df6e",
-    "url": "/jwpp/static/js/main.337beaf6.chunk.js"
+    "revision": "b171018926622d49d988",
+    "url": "/jwpp/static/js/main.b1710189.chunk.js"
   },
   {
     "revision": "6465c6654c873752aca1",
     "url": "/jwpp/static/js/1.6465c665.chunk.js"
   },
   {
-    "revision": "337beaf638115907df6e",
+    "revision": "b171018926622d49d988",
     "url": "/jwpp/static/css/main.75fdf33e.chunk.css"
   },
   {
-    "revision": "30b9e5da416b06dfd2023deeefce6c47",
+    "revision": "eb21202cdf4bebd604cc14440c92bfc2",
     "url": "/jwpp/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/jwpp/precache-manifest.682bb730c03fec7b765691b9f0503bc0.js"
+  "/jwpp/precache-manifest.3948bcff9936b4393f20def58a771e13.js"
 );
 
 workbox.clientsClaim();
