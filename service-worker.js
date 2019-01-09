b075d264aa187c54d17c3f7467f253b29160244c
jongwoo <naace910@gmail.com>
2019-01-09 13:41:45 +0900

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/jwpp/precache-manifest.0a981ca81c34cb0f5053efdd454b94ad.js"
+  "/jwpp/precache-manifest.0371c959db11ed61efab7f3d6f2e3a07.js"
 );
 
 workbox.clientsClaim();
